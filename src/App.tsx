24b56a13c6ffb9ee318ea7244f1bd36f88e2c6e1
Roel Fajardo <rfajardo1026@gmail.com>
2023-10-19 13:54:17 +0800

    Testing the git

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -17,9 +17,13 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h3>{count}</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Click to ADD count
+        </button>
+        <button onClick={() => setCount((count) => count - 1)}>
+          Click to DEDUCT count
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
@@ -29,7 +33,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
