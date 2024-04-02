80937359c7bf9ce1b3ce969de86b133ec2bd31c2
Roel Fajardo <rfajardo1026@gmail.com>
2024-04-02 21:36:39 +0800

    Build color mode switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import pokemonLogo from "../assets/pokemon_logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 export default function NavBar() {
   return (
-    <HStack>
+    <HStack justifyContent={"space-between"} padding="10px">
       <Image src={pokemonLogo} boxSize="60px" width="90px" />
-      <Text>NavBar</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 }
